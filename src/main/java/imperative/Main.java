c69da19d3c0ace096535803c87e7d4bfc2063893
veeko18 <45859068+veeko18@users.noreply.github.com>
2020-06-30 16:35:36 +0300

    error with arraylist

---
+++ src/main/java/imperative/Main.java
@@ -0,0 +1,47 @@
+package imperative;
+
+import com.sun.tools.javac.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("John", Gender.MALE),
+                new Person("Maria", Gender.FEMALE),
+                new Person("Aisha", Gender.FEMALE),
+                new Person("Alex", Gender.MALE),
+                new Person("Alice", Gender.FEMALE)
+        );
+
+        //imperative approach
+        List<Person> females = new ArrayList<>();
+
+        for(Person person : people){
+            if(person.equals(Gender.FEMALE)){
+                females.add(person);
+            }
+        }
+    }
+
+    static class Person{
+        private final String name;
+        private final Gender gender;
+
+        public Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender{
+        FEMALE, MALE
+    }
+}
